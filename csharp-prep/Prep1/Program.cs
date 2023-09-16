1ce65aa84f47cbf2d5af084603067062ef87b77e
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-09-16 00:13:25 -0700

    This is my first update to prep 1, code works

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name?: ");
+        string first = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.WriteLine("What is your Last Name?: ");
+        string last = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+        Console.WriteLine();
+    
     }
 }

dba1a2fc56a18d1eca472c01faec1cd1db032028
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-09-16 12:10:57 -0700

    First code push for Prep2 and sandbox

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // Goal of this code is write a program takes the users grade input and lets them know if they passed the class. Use a series of if else statements. 
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your Grade Number: ");
+        string valveFromUser = Console.ReadLine();
+
+        int x = int.Parse(valveFromUser);
+        int a = 90;
+        int b = 80;
+        int c = 70;
+        int d = 60;
+
+
+        if (x >= a)
+        {
+            Console.WriteLine("Great you got an A!");
+        }
+        else if (x >= b)
+        {
+            Console.WriteLine("B you are going good! Keep up the Work");
+        }
+        else if (x >= c)
+        {
+            Console.WriteLine("C Your doing good, remember to ask your Professor if you need Help!");
+        }
+        else if (x >= d)
+        {
+            Console.WriteLine("D Oh no, remember you have resources to help your grade, Ask your Professor");
+        }
+        else
+        {
+            Console.WriteLine("F Oh no, Please reach out for help, Your Professor is a great place to start!");
+        }
+
+
+
+   
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("Enter a number: ");
+        string valveFromUser = Console.ReadLine();
+        
+        int x = int.Parse(valveFromUser); 
+        int y = 4;
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if(x < y)
+        {
+            Console.WriteLine("Less");
+        }
+        else 
+        {
+            Console.WriteLine("Equal");
+        }
+        // End of how the if and else statements work. 
+
+
     }
 }

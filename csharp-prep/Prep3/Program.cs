8e6a9350ff7ebf9f44f298c44898e31a66ddd3d7
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-09-20 16:05:31 -0700

    First test run at while loop, it worked, had no way to exit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -8,5 +8,14 @@ class Program
 
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(1,11);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your Guess:  ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
     }
 }

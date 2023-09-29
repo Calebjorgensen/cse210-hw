eae3b68357b77460501cabe927601e006c200d7e
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-09-28 20:10:47 -0700

    Added a display, both jobs work

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,7 +9,10 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
 
+        Job job2 = new Job();
+        job2._jobTitle = "Computer Programmer";
 
         Console.WriteLine(job1._jobTitle);
+        Console.WriteLine(job2._jobTitle);
     }
 }

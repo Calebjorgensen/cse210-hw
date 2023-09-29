c67d9914ab3e5b58be9c81619cdfb252670b25d2
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-09-28 20:21:42 -0700

    Finish the Job class everything displays

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,10 +9,14 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Apple";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
 
         Job job2 = new Job();
         job2._jobTitle = "Computer Programmer";
         job2._company = "Microsoft";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
 
         job1.Display();
         job2.Display();

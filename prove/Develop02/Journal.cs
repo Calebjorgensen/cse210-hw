6289bb94403eb0d0ae3c2965a4b212b9d13e6a6d
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-05 09:08:32 -0700

    Set up journal Class and switch case test

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Journal
+{
+    public string _display;
+    public string _load;
+    public string _save;
+    public string _exit;
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+        int choice = 1;
+
+        switch(choice)
+        {
+            case 1:
+            Console.WriteLine("Test Case 1");
+            break;
+
+            case 2:
+            Console.WriteLine("Test Case 2");
+            break;
+        }
     }
 }

d9502bf486cb6b1e4e34ab09700242f06297b8c4
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-06 10:41:10 -0700

    starting off with my new build

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,50 +8,7 @@ class Program
     static void Main(string[] args)
     {
         
-        
-            //I AM GOING TO CHANGE THIS CODE AND START OVER.
-
-
-        string userinput;
-        Entry input = new Entry();
-
-        Console.WriteLine("Welcome to your Journal");
-        
-        do
-        {
-            Console.WriteLine("Please select one of the following choices");
-            Console.WriteLine("1. Write Entry: ");
-            Console.WriteLine("2. Display Journal: ");
-            Console.WriteLine("3. Load Journal: ");
-            Console.WriteLine("4. Save Journal: ");
-            Console.WriteLine("5. Exit");
-            userinput = Console.ReadLine();
-
-            switch(userinput)
-            {
-                //Choice 1 New Entry
-                case "1":
-                break;
-
-                //Choice 2 Display Journal
-                case "2":
-                break;
-
-                //Save Journal
-                case "3":
-                break;
-
-                //Load Journal
-                case "4":
-                break;
-
-                //No case for option 5 as its the last one outside of the switch case.
-            }
-
-
-
-
-        }while(userinput != "0");
+        Console.WriteLine("Hello Prove # 2. I am going to code the crap out of you!");
        
     }
 }

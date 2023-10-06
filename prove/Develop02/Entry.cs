d52fb7f7a6c9df34a54b1dc9f58d3a46f34cdae6
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-06 11:05:45 -0700

    Added my text to the main

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -6,6 +6,7 @@ public class Entry
     public string _getPrompt;
     public string _submit;
 
+
     
 
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,7 +8,18 @@ class Program
     static void Main(string[] args)
     {
         
+        string choice;
+
         Console.WriteLine("Hello Prove # 2. I am going to code the crap out of you!");
-       
+        Console.WriteLine("");
+        Console.WriteLine("Please select one of the options below");
+        Console.WriteLine("1. New Entry: ");
+        Console.WriteLine("2. Display Entries: ");
+        Console.WriteLine("3. Load Journal; ");
+        Console.WriteLine("4. Save Journal: ");
+        Console.WriteLine("5. Exit ");
+        choice = Console.ReadLine();
+
+        
     }
 }

bff87c1d49dcd3e0480d2e573fb77156edf03545
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-06 16:59:48 -0700

    no real changes

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,36 +1,42 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
-
+// Class Name Entry
 public class Entry
 {
+/* Variables for this class are, newEntry, getPrompt, and sumbit
+_newEntry is to record the new journal entyr of the user
+_getPrompt is to randomize a prompt for the new entry
+_submit not sure what the sumbut is suppose to do?? 
+
+*/
     public string _newEntry;
     public string _getPrompt;
     public string _submit;
 
+// This method was in the example for Abstractions, said it was a speical constructor.
 public Entry()
 {
     
 }
 
+//This Method is so that I can Display the _newEntry, and the _getPrompt
 public void Display()
 {
     Console.WriteLine($"{_newEntry}");
     Console.WriteLine($"{_getPrompt}");
 }    
 
-
-public static string [] prompt = 
-{
-    "How have you seen the Hand of God in your life today?",
-    "What was 3 postive things that happened today?",
-};
-
-
+//Goal of this Method is to create a list of prompts and have them random generate. 
+// Has not worked yet. 
 public void PromptGenerator()
 {
-    Console.WriteLine($"{prompt}");
+
+    Console.WriteLine($"{_getPrompt}");
 }
+
+
 
 }
 

1313bf49e6a2b57b4dc341e7801d34db545e9b45
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-05 19:26:36 -0700

    added switch to main program

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -7,13 +7,7 @@ public class Journal
     public string _save;
     public string _exit;
 
-    public void Display()
-    {
-        Console.WriteLine($"{_display}");
-        Console.WriteLine($"{_load}");
-        Console.WriteLine($"{_save}");
-        Console.WriteLine($"{_exit}");
-    }
+
 
 
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,40 +1,52 @@
 using System;
+using System.Xml.Serialization;
+
 
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string userinput;
+        Entry input = new Entry();
 
-       Journal journal1 = new Journal();
-       journal1._display = "1. Display";
-       journal1._load = "2. Load";
-       journal1._save = "3. Save";
-       journal1._exit = "4. Exit";
-       journal1.Display();
-       
-       /* int choice = 3;
-
-        switch(choice)
-        {
-            case 1:
-            Console.WriteLine("Test Case 1");
-            break;
-
-            case 2:
-            Console.WriteLine("Test Case 2");
-            break;
-
-            case 3:
-            Journal journal1 = new Journal();
-            journal1._display = "Display";
-            break;
-
-        }
-        */
-
+        Console.WriteLine("Welcome to your Journal");
         
+        do
+        {
+            Console.WriteLine("Hello there");
+            Console.WriteLine("1. Enter Entry: ");
+            Console.WriteLine("2. Display Journal: ");
+            Console.WriteLine("3. Save Journal: ");
+            Console.WriteLine("4. Load Journal: ");
+            Console.WriteLine("5. Exit");
+            userinput = Console.ReadLine();
 
+            switch(userinput)
+            {
+                //Choice 1 New Entry
+                case "1":
+                break;
+
+                //Choice 2 Display Journal
+                case "2":
+                break;
+
+                //Save Journal
+                case "3":
+                break;
+
+                //Load Journal
+                case "4":
+                break;
+
+                //No case for option 5 as its the last one outside of the switch case.
+            }
+
+
+
+
+        }while(userinput != "0");
+       
     }
 }

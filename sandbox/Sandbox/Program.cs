ac669674e764a328cb23e3f0dcc55f90af0ae851
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-06 22:17:07 -0700

    Working on Methods

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,10 +6,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+       
 
         Welcome("Caleb");
         Welcome("Elle");
         Lesson();
+        Color();
+        Input();
+        
     }
 
     static void Welcome(string name )
@@ -20,7 +24,16 @@ class Program
     static void Lesson()
     {
         Console.WriteLine("Lesson One is covering Classes");
-        Console.WriteLine("");
         Console.WriteLine("Please begin to take notes");
+    }
+
+    static void Color()
+    {
+        Console.WriteLine("The best Color is Yellow");
+    }
+
+    static void Input()
+    {
+        Console.WriteLine("What am I doing in this class?");
     }
 }

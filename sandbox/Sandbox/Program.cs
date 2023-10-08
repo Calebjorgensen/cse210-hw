b2051ce9a7fde605c504469de9dd80da8ff2edfe
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-07 18:22:34 -0700

    clear sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,29 +7,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+    }
        
-
-        Welcome("Caleb " + "Elle");
-        Number();
-        
-    }
-
-    static void Welcome(string name )
-    {
-        Console.Write("Welcome, " + name);
-        Console.WriteLine(" I am learning about Methods");
-    }
-    static int Number()
-    {
-        int number = 9;
-        if (number > 8)
-        {
-            Console.WriteLine("Congrats");
-        }
-        else
-        {
-            Console.WriteLine("Try again");
-        }
-        return number;
-    }
 }

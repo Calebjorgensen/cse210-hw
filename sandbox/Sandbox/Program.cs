d185dbbd65846bdf1bd1d99d38ca68c87d8cc52a
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-07 18:30:32 -0700

    added a test class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections;
 
 
+class Foo
+{
+    public string _bar; // An Attribute
+
+    public void Display()
+    {
+        Console.WriteLine($"{_bar}");
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Foo barz; // An Object
+        barz = new Foo(); // Instantation
+        barz._bar = "Hello World";
+        barz.Display();
+
     }
        
 }

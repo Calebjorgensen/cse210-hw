fd27f49ebc703be93f18a8895c11e4d9aeb679fa
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-07 21:58:14 -0700

    try to add the save file function

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -20,11 +20,20 @@ public class Journal
 
 public void SaveFile()
 {
-    
+    string filename = "myFile.txt";
+    string[] lines = System.IO.File.ReadAllLines(filename);
+
+    foreach (string line in lines)
+    {
+        string[] parts = line.Split(",");
+
+        string firstName = parts[0];
+        string lastName = parts[1];
+}
 }
 public void LoadFile()
 {
-    
+
 }
 
 }

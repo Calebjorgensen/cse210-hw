3e689e09ae0443e88d4fff40a33591d828be8a03
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-09 11:22:16 -0700

    starting working a menu program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,11 +1,31 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
+
+class Food
+{
+    public string _apps;
+    //public string _entree;
+    //public string _desserts;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_apps}");
+    }
+
+}
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Food apps = new Food();
+        apps._apps = "Cheese Curds";
+        apps.Display();
+
 
 
     }

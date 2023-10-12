c12a1430256ce1ee0bb701ded088589cb12f7a13
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-12 14:33:15 -0700

    simple if statement

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,12 +2,19 @@ using System;
 
 
 
+
 class Program
 {
     static void Main(string[] args)
     {
 
+        int a = 6;
+        int b= 78;
 
+        if( a < b )
+        {
+           Console.WriteLine("True");
+        }
 
     }
        

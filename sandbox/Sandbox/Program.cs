902563002a15249059cb5d6b9778b82d5dabb569
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-12 09:26:41 -0700

    practice private varibles

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,7 +1,20 @@
 using System;
 
 
+public class Person
+{
+    private string _firstName;
 
+    public string GetFirstName()
+    {
+        return _firstName;
+    }
+
+    public void SetFirstName(string firstName)
+    {
+        _firstName = firstName;
+    }
+}
 
 
 
@@ -11,7 +24,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Person p = new Person();
+        p.SetFirstName("Caleb");
 
+        Console.WriteLine(p.GetFirstName());
 
 
     }

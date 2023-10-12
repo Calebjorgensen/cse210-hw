bd65f03c6032dd1f135aa5b0ba6b643ad85d40de
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-12 10:18:24 -0700

    reset the program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,33 +1,12 @@
 using System;
 
 
-public class Person
-{
-    private string _firstName;
-
-    public string GetFirstName()
-    {
-        return _firstName;
-    }
-
-    public void SetFirstName(string firstName)
-    {
-        _firstName = firstName;
-    }
-}
-
-
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Person p = new Person();
-        p.SetFirstName("Caleb");
 
-        Console.WriteLine(p.GetFirstName());
 
 
     }

96783e59e2fe8b66e12be2004d55758152d77fd5
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-17 09:55:52 -0700

    simple program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,12 +2,25 @@ using System;
 
 
 
+public class Soups
+{
+    public string _warmSoups;
+    public string _coldSoups;
+
+    public void Dispay()
+    {
+        Console.WriteLine($"{_warmSoups}");
+        Console.WriteLine($"{_coldSoups}");
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-
-        Console.WriteLine("Hello World");
+        Soups s = new Soups();
+        s._warmSoups = "Broccoli and Cheese soup";
+        s._coldSoups = "Thai soup";
+        s.Dispay();
     }
        
 }

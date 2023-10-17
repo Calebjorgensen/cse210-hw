f38f73cf992f0aa440e9449f603a1464787c38b8
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-17 09:26:48 -0700

    reset

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,27 +2,12 @@ using System;
 
 
 
-public class Calculator
-{
-    public int _add;
-
-
-    public void Math()
-    {
-       string input = Console.ReadLine();
-       Console.WriteLine(input);
-    }
-}
-
-
-
 class Program
 {
     static void Main(string[] args)
     {
-        Calculator m = new Calculator();
-        m.Math();
 
+        Console.WriteLine("Hello World");
     }
        
 }

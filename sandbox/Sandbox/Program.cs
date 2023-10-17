c0b868001d9e4676772214d2922a1ac78cd725b4
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-16 19:44:03 -0700

    this is another test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,39 +1,16 @@
 using System;
-using System.Diagnostics.Contracts;
-using System.Xml.XPath;
+
 
 
 public class Calculator
 {
     public int _add;
 
-    public void Addiction()
+
+    public void Math()
     {
-        int x = 10;
-        int y = 9;
-
-        int answer = x + y;
-
-        Console.WriteLine(answer);
-        
-    }
-
-    public void Substraction()
-    {
-        int s = 73;
-        int a = 10;
-
-        int answer2 = s - a;
-        Console.WriteLine(answer2);
-    }
-
-    public void multiplication()
-    {
-        int t = 5;
-        int m = 5;
-
-        int answer3 = t * m;
-        Console.WriteLine(answer3);
+       string input = Console.ReadLine();
+       Console.WriteLine(input);
     }
 }
 
@@ -44,9 +21,8 @@ class Program
     static void Main(string[] args)
     {
         Calculator m = new Calculator();
-        m.Addiction();
-        m.Substraction();
-        m.multiplication();
+        m.Math();
+
     }
        
 }

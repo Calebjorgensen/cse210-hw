3d8186332ad39cd037817fb7808d4e80206ed05f
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-19 16:40:26 -0700

    a array loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,60 +1,22 @@
 using System;
 
-public class Hose
-{
-    private string _size;
-    private string _length;
-    private string _color;
 
-    public void ShowSizes()
-    {
-        Console.WriteLine($"{_size}");
-    }
-
-    public void ShowLengths()
-    {
-        Console.WriteLine($"{_length}");
-    }
-
-    public void ShowColors()
-    {
-        Console.WriteLine($"{_color}");
-    }
-
-    public string GetInfoC
-    {
-        get {return _color;}
-        set {_color = value;}
-    }
-    public string GetInfoL
-    {
-        get {return _length;}
-        set {_length = value;}
-    }
-    public string GetInfoS
-    {
-        get {return _size;}
-        set {_size = value;}
-    }
-
-}
 
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Hose s = new Hose();
-        s.GetInfoS = "1/4, 1/2, 3/4, 1";
-        s.ShowSizes();
+    {  
 
-        Hose l = new Hose();
-        l.GetInfoL = "25ft, 50ft, 75ft, 100ft";
-        l.ShowLengths();
-
-        Hose c = new Hose();
-        c.GetInfoC = "Red, Black, Blue, White, Yellow, Green";
-        c.ShowColors();
+        int [] a = new int [10];
+        for (int k = 0; k < 10; k ++)
+        {
+            a[k] = k*2;
+        }
+        for (int k = 0; k < 10; k ++)
+        {
+            Console.WriteLine(a[k]);
+        }
     }
        
 }

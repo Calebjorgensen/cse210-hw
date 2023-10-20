4802983ca41c694358a631981699d93cf53e7cea
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-20 09:06:49 -0700

    simple variable program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,12 +8,8 @@ class Program
     static void Main(string[] args)
     {  
 
-        int [] a = new int [20];
-        for (int k = 0; k < 20; k ++)
-        {
-            a[k] = k*2;
-            Console.WriteLine(a[k]);
-        }
+        string name = "Caleb";
+        Console.WriteLine(name);
 
     }
        

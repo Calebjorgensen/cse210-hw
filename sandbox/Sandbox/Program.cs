e780502eb97c4183a35ec15d0bf25ad3656d5955
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-21 10:41:48 -0700

    added some comments

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,12 +7,15 @@ class Program
 {
     static void Main(string[] args)
     {  
+        // This is getting the input from the user
         string input1 = Console.ReadLine();
         string input2 = Console.ReadLine();
 
+        // This is converting the string input into an Int
         int number1 = Convert.ToInt32(input1);
         int number2 = Convert.ToInt32(input2);
 
+        // Adding the too new Ints 
         int answer = number1 + number2;
 
         Console.WriteLine(answer);

57e52034f19933170bb25b73b1548380acb11cbb
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-21 10:44:35 -0700

    shorted the code but still works

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,13 +7,10 @@ class Program
 {
     static void Main(string[] args)
     {  
-        // This is getting the input from the user
-        string input1 = Console.ReadLine();
-        string input2 = Console.ReadLine();
 
-        // This is converting the string input into an Int
-        int number1 = Convert.ToInt32(input1);
-        int number2 = Convert.ToInt32(input2);
+        
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
 
         // Adding the too new Ints 
         int answer = number1 + number2;

55f2d84f05aee5db2d873de7db05ddcba7d6d6b1
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-10-21 10:40:23 -0700

    Build a simple input calculator

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,13 +7,15 @@ class Program
 {
     static void Main(string[] args)
     {  
+        string input1 = Console.ReadLine();
+        string input2 = Console.ReadLine();
 
-        int price = 30;
-        int discount = 2;
+        int number1 = Convert.ToInt32(input1);
+        int number2 = Convert.ToInt32(input2);
 
-        price /= discount;
+        int answer = number1 + number2;
 
-        Console.WriteLine(price);
+        Console.WriteLine(answer);
         
 
     }

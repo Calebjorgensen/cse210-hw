680df01a75a5e9fe645514f3e66db2c30e2d88f0
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-10-30 08:55:53 -0700

    This is a while loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,11 +8,14 @@ class Program
     static void Main(string[] args)
     {  
 
-        
-        for (int x = 5; x >= 1; x--)
+        int x = 0;
+
+        while(x<=21)
         {
             Console.WriteLine(x);
+            x+=3;
         }
+
 
     }
        

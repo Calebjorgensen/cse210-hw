fea6bbd6141fc94b35ee5bc5e6a26183e922c792
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-03 12:19:06 -0700

    working on methods

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,12 +2,18 @@ using System;
 
 class Bank
 {
-    //public string _email;
+    public string _email;
 
     // This is a Constructor. The Idea of this is to do less code to get the same result.
     public Bank()
     {
         Console.WriteLine("Send Email to user");
+    }
+
+    public string Email(string _email)
+    {
+        Console.WriteLine(_email);
+        return _email;
     }
 }
 

9a692270ad19e40842ccd9494410c926caebc660
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-03 14:57:56 -0700

    cleaned up prorgom

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,25 +1,13 @@
 using System;
 
 
-class Person
-{
-    private string _name;
-    public string Name
-    {
-        get {return _name;}
-        set {_name = value;}
 
-    }
-}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Person p = new Person();
-        p.Name = "Caleb";
-        Console.WriteLine(p.Name);
-
+        Console.WriteLine("Hello World");
     }    
 
 

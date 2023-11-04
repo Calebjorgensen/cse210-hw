40b15e81418340422f14921b2be44674bc693952
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-11-04 14:49:22 -0700

    program for a menu, has a contructor and method

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Runtime.CompilerServices;
 
-class Calculator
+
+class Menu
 {
-   // public int _add;
-    //public int _sub; 
-    //public int _times;
-    //public double _division;
+    public string _apps;
+    public string _eentres;
+    public string _deserts;
 
-    public Calculator() // Contructor
+    public Menu()
     {
-        Console.WriteLine("Hello welcome to this app");
+        Console.WriteLine("Welcome to Broken Barrell");
+        Console.WriteLine("Heres our menu");
+        Console.WriteLine("");
+    }
+
+    public void menuList()
+    {
+        Console.WriteLine($"appetizers: {_apps}\nEetnres: {_eentres}\nDeserts: {_deserts}");
     }
 }
 
@@ -18,42 +26,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Calculator c = new Calculator();
-
-        int choice;
-
-        do
-        {
-
-            Console.WriteLine("1. For Addiction");
-            Console.WriteLine("2. For Substraction");
-            Console.WriteLine("3. For times");
-            Console.WriteLine("4. for division");
-            Console.WriteLine("5. To Exit");
-
-            choice = Convert.ToInt32(Console.ReadLine());
-
-
-        
-        switch(choice)
-        {
-            case 1:
-            Console.WriteLine("Case 1");
-            break;
-
-            case 2:
-            Console.WriteLine("Case 2");
-            break;
-
-            case 3:
-            Console.WriteLine("Case 3");
-            break;
-
-            case 4:
-            Console.WriteLine("Case 4");
-            break;
-        }
-        } while(choice != 5);
+        Menu m = new Menu();
+        m._apps = "Cheese Sticks";
+        m._eentres = "Lets go nuts burger";
+        m._deserts = "milkshakes";
+        m.menuList();
+      
     }    
 
 

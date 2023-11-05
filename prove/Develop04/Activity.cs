41cf1aa19009e98e0ddf466f887a172a9f2c3876
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-11-04 18:14:25 -0700

    The first part of the constructer works. it asks for time

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -5,6 +5,13 @@ class Activity
     public int _timer;
     public string _name;
 
+
+    public Activity()
+    {
+        Console.WriteLine("Enter the time in seconds your want to do this activity");
+        _timer = int.Parse(Console.ReadLine());
+        Console.WriteLine("You have enter " + _timer + " in seconds");
+    }
     public void startingActivity()
     {
         Console.WriteLine("Hello");

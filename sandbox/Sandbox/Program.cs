c1dc49b1cf92dac5a9e1d8cd17816190bde1a172
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-07 12:15:50 -0800

    first part of the class works

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,5 +1,32 @@
 using System;
-using System.ComponentModel.Design;
+
+
+
+class Calculator
+{
+    public int _add;
+    public int _sub;
+    public int _mult;
+    public double _div;
+
+    public Calculator()
+    {
+        Console.WriteLine("This is a Calculator Program");
+
+    }
+
+    public int Addition()
+    {
+        int x = 4;
+        int y = 5;
+
+        int sum = x + y;
+        return sum;
+        
+    }
+
+}
+
 
 
 
@@ -10,9 +37,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-
-
+        Calculator add = new Calculator();
+        add.Addition();
+        Console.WriteLine(add.Addition());
 
     }    
 

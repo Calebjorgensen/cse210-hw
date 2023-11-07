a5e6b9d751f8c340f302b04df73996dd3b44922d
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-07 11:55:23 -0800

    clearing the code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,27 +1,7 @@
 using System;
 using System.ComponentModel.Design;
 
-class Entry
-{
 
-
-    public Entry()
-    {
-        Console.WriteLine("Welcome to Sammy's Food Shack");
-    }
-    public string [] menu = {"soup","crab","steak","desert"};
-    
-    
-    public void Display()
-    {
-
-    }
-}
-
-class Calculator
-{
-
-}
 
 
 
@@ -30,18 +10,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
-        Entry e = new Entry();
+        
 
-        int [] numbers = {5,6,7,8,9};
-        int sum = 0;
 
-        foreach(int x in numbers)
-        {
-            sum += x;
-        }
-        Console.WriteLine(sum);
-      
+
     }    
 
 

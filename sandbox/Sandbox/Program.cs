2ef5b01bed720248a0d2940865b188852b6176e9
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-13 09:12:01 -0800

    Resetting the sandbox project file

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,21 +3,13 @@ using System;
 
 
 
+
 class Program
 {
     static void Main(string[] args)
     {
 
-        int number = 0;
-
-        do
-        {
-            Console.WriteLine(number);
-
-            number++;
-        }while(number<5);
-
-        Console.WriteLine("Loop Finished");
+        Console.WriteLine("Hello World");
 
     }    
 }

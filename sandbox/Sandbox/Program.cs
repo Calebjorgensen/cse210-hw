5c51e8416a10dda810574e27d75dbe54e17563d6
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-13 10:17:31 -0800

    Simple guessing game

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,11 +2,29 @@ using System;
 
 
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Hello there");
+
+        int answer = 14;
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        if(answer > choice)
+        {
+            Console.WriteLine("Too Low, try again");
+        }
+        else if(answer < choice)
+        {
+            Console.WriteLine("Too high, try again");
+        }
+        else
+        {
+            Console.WriteLine("That is right!");
+        }
         
     }    
 }

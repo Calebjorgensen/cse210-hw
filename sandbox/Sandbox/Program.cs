f7248e617a5a080be07b93db50ac86cc857f0bf6
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-13 10:26:07 -0800

    This is just a reet

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,28 +3,12 @@ using System;
 
 
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello there");
-
-        int answer = 14;
-
-        int choice = Convert.ToInt32(Console.ReadLine());
-
-        if(answer > choice)
-        {
-            Console.WriteLine("Too Low, try again");
-        }
-        else if(answer < choice)
-        {
-            Console.WriteLine("Too high, try again");
-        }
-        else
-        {
-            Console.WriteLine("That is right!");
-        }
+        Console.WriteLine("Hello There");
         
     }    
 }

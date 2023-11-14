715746643e50955f192c029962e6598ff1e398a8
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-11-13 19:04:15 -0800

    reset

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,28 +1,11 @@
 using System;
 
 
-class Time
-{
-    public Time()
-    {
-        Console.WriteLine("Welcome");
-    }
-
-    public void Date()
-    {
-        DateTime currentDate = DateTime.Now;
-        Console.WriteLine(currentDate);
-
-    }
-}
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        Time t = new Time();
-        t.Date();
-        
+        Console.WriteLine("Hello World");
     }    
 }

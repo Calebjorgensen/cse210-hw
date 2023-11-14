5e689974b2898f5059299df0ad4473ea6cc8fe03
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-11-13 20:19:25 -0800

    Made a multi class program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,11 +1,35 @@
 using System;
 
 
+class Entry
+{
+    public string user;
+    public string date;
+
+    public void Info()
+    {
+        Console.WriteLine($"{user} ~ {date}");
+    }
+}
+
+
+class Time
+{
+    public void Date()
+    {
+        Entry e = new Entry();
+        DateTime currentDateTime = DateTime.Now;
+        e.date = currentDateTime.ToShortDateString();
+        e.user = "Caleb";
+        e.Info();
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        Time t = new Time();
+        t.Date();
     }    
 }

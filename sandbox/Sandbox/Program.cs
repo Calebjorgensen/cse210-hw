7abd6f9b3ffbd42a250164661b7393e3953e40b0
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-15 09:07:11 -0800

    Reset to the program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,54 +1,10 @@
 using System;
 
 
-
-class Entry
-{
-    public string user;
-    public string date;
-
-    public void Info()
-    {
-        Console.WriteLine($"{user} ~ {date}");
-    }
-}
-
-class Math
-{
-    public int Add()
-    {
-        int x = Convert.ToInt32(Console.ReadLine());
-        int y = Convert.ToInt32(Console.ReadLine());
-
-        int sum = x + y;
-        Console.WriteLine(sum);
-        return sum;
-    }
-}
-
-
-class Time
-{
-    public void Date()
-    {
-
-        Entry e = new Entry();
-        DateTime currentDateTime = DateTime.Now;
-        e.date = currentDateTime.ToShortDateString();
-        e.user = Console.ReadLine();
-        e.Info();
-
-
-        Math m = new Math();
-        m.Add();
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
     {
-        Time t = new Time();
-        t.Date();
+        Console.WriteLine("Hello World");
     }    
 }

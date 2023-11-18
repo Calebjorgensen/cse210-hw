6149029acb64234e738ab4c09c037996a9a6d993
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2023-11-18 11:00:20 -0800

    Made my menu in program

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int user;
+
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Create new Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+
+            user = Convert.ToInt32(Console.ReadLine());
+
+            switch(user)
+            {
+                case 1:
+                Console.WriteLine("");
+                break;
+
+                case 2:
+                Console.WriteLine("");
+                break;
+
+                case 3:
+                Console.WriteLine("");
+                break;
+
+                case 4:
+                Console.WriteLine("");
+                break;
+
+                case 5:
+                Console.WriteLine("");
+                break;
+            }
+
+        }
     }
 }

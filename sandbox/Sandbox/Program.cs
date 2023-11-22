9ded77799b0496ce2ddf1ea9a5db499766b4c157
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-21 16:05:28 -0800

    creating a class to work saving files

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,34 @@
 using System;
 
+class Person
+{
+    public string _firstName;
+    public string _lastName;
+    public int _age;
+}
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hi Seth");
+        Person p1 = new Person();
+        p1._firstName = "Caleb";
+        p1._lastName = "Jorgensen";
+        p1._age = 24;
+
+        Person p2 = new Person();
+        p2._firstName = "Ellexis";
+        p2._lastName = "Buck";
+        p2._age = 28;
+
+        List<Person> people = new List<Person>();
+        people.Add(p1);
+        people.Add(p2);
+
+        foreach (Person p in people)
+        {
+            Console.WriteLine(p._firstName);
+        }
     }    
 }

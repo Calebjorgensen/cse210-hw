3dfa4893f42690291efbd1b8110721afffa59af4
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2023-11-30 14:48:25 -0800

    added some stuff to anouther class

---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,7 @@
+class Reception : Event
+{
+    public Reception(string title, string desc, string date, string time, Address address) : base (title, desc, date, time, address)
+    {
+        
+    }
+}
